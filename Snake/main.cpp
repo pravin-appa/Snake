d211519d1bc49296a7a68565abb4ad91a296deef
Pravin Suryawanshi <spravin20032001@gmail.com>
2021-10-22 20:28:36 +0530

    Source files

---
+++ Snake/main.cpp
@@ -0,0 +1,33 @@
+#include <SDL.h>
+#undef main
+#include "Game.h"
+
+
+
+
+
+
+int main()
+{
+	
+	Game game;
+	
+
+	game.init("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 700, 700, false);
+
+	while (game.running())
+	{
+		game.handleEvents();
+		game.update();
+		game.render();
+	
+		
+	}
+
+	
+	game.clean();
+	
+	
+
+	return 0;
+}
